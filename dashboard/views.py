05c49845ea3641fb2455d8bbfcd6aef9e80b3ab5
niyirerayannick <niyannick120@gmail.com>
2026-04-23 18:53:37 +0200

    yuuu

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 
+from accounts.models import SystemSettings
 from accounts.mixins import DashboardRoleMixin
+from accounts.currency import CURRENCY_SYMBOLS
 
 from .services import dashboard_snapshot
 
@@ -10,10 +12,18 @@ class DashboardHomeView(DashboardRoleMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        settings = SystemSettings.get_settings()
+        currency_code = getattr(settings, "currency", "USD") or "USD"
+        currency_symbol = (
+            getattr(settings, "currency_symbol", "")
+            or CURRENCY_SYMBOLS.get(currency_code, currency_code)
+        )
         context.update(
             {
                 "page_title": "Dashboard",
                 "active_menu": "dashboard",
+                "dashboard_currency_code": currency_code,
+                "dashboard_currency_symbol": currency_symbol,
                 **dashboard_snapshot(),
             }
         )
